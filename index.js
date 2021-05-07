e5e6f503ee436850e0ec7307c389f0b3c156cee1
Marvin Wu <marvin.wu@urbanspring.hk>
2021-05-07 12:46:06 +0800

    ttt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const SaveToExistingDirectoryPlugin = require('website-scraper-existing-director
 const {urlTypeFilter} = require('./util')
 
 
-const targetUrl = 'https://www.succulentsandsunshine.com/'
+const targetUrl = 'https://worldofsucculents.com/'
 
 const options = {
   urls: [targetUrl],
